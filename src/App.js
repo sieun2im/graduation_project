42b84e59b9628910064e12a47ce4425b3b277e31
biyuns <cart0089@naver.com>
2025-11-06 17:19:13 +0900

    dkfjdkfj

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
 import Main from './components/main'
 function App() {
+    useEffect(() => {
+    function openFullscreen() {
+      const elem = document.documentElement;
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { // Safari
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { // IE11
+        elem.msRequestFullscreen();
+      }
+    }
+
+    // 화면 클릭할 때 전체화면 실행
+    document.documentElement.addEventListener('click', openFullscreen);
+
+    // 컴포넌트 언마운트 시 이벤트 제거
+    return () => {
+      document.documentElement.removeEventListener('click', openFullscreen);
+    };
+  }, []);
   return (
     <div className="app">
       <Main/>
